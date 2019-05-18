f2177937147643c165f6f063106b41556b43b1c7
tushar-semwal <semwaltushar@gmail.com>
2019-05-18 12:37:38 +0100

    update

--- auto_push.sh
+++ auto_push.sh
@@ -1,12 +1,10 @@
 hugo
 git add .
 git commit -m "update"
-git push origin HEAD:master --force
-#git push -u origin master
+git push -u origin master
 
 cd public
 git add .
 git commit -m "build"
-git push origin HEAD:master --force
-#git push origin master
+git push origin master
 cd ..
